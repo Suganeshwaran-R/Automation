648a25e4fc771c982e7e7aab6b784df0b1d4b2be
LENOVO <sathish03ece@gmail.com>
2024-12-20 10:15:34 +0530

    Redirect the Project

--- Automation/src/practice/AuthenticationPopup.java
+++ Automation/src/practice/AuthenticationPopup.java
@@ -8,6 +8,5 @@ public static void main(String[] args) {
 	WebDriver d=new ChromeDriver(); 
     d.manage().window().maximize();
     d.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
-    
-}
+    }
 }
